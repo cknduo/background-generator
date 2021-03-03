d6523b56eeff3f295b071ceb4ec9138f0565f4cd
cknduo <chloeesilvester@gmail.com>
2021-03-03 13:39:00 -0700

    update

--- bundle.js
+++ bundle.js
@@ -1,5 +1,4 @@
-var _ = require('lodash');
-
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
@@ -44,3 +43,5 @@ function setRandom() {
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 random.addEventListener("click", setRandom);
+
+},{}]},{},[1]);
